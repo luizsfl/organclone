5145c164c8ec77f621703e6e257c5f8b480929ac
Pedro Luiz <luizsfl@hotmail.com>
2022-05-10 14:30:06 -0300

    Criação do resumo da tela principal

--- app/src/main/java/pedro/projeto/organclone/activity/PrincipalActivity.kt
+++ app/src/main/java/pedro/projeto/organclone/activity/PrincipalActivity.kt
@@ -12,20 +12,34 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import pedro.projeto.organclone.R
 import pedro.projeto.organclone.config.ConfiguracaoFirebase
 import pedro.projeto.organclone.databinding.ActivityPrincipalBinding
+import pedro.projeto.organclone.helper.Base64Custom
+import pedro.projeto.organclone.model.Usuario
+import java.text.DecimalFormat
 
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var calendarView : MaterialCalendarView
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPrincipalBinding
-private lateinit var textoSaldo:TextView
-private lateinit var textoSaldacao:TextView
-private lateinit var toolbar: androidx.appcompat.widget.Toolbar
-private lateinit var autenticacao : FirebaseAuth
+    private lateinit var textoSaldo:TextView
+    private lateinit var textoSaldacao:TextView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private var autenticacao : FirebaseAuth = ConfiguracaoFirebase.getFirebaseAutenticacao()
+    private var firebaseRef : DatabaseReference = ConfiguracaoFirebase.getFirebaseDataBse()
+    private  var despesaTotal: Double = 0.0
+    private  var receitaTotal: Double = 0.0
+    private  var resumoUsuario: Double = 0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -47,13 +61,8 @@ private lateinit var autenticacao : FirebaseAuth
         textoSaldo = findViewById(R.id.txtSaldo)
         textoSaldacao = findViewById(R.id.txtSaldacao)
 
+        recuperarResumo()
 
-    /*
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        */
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -87,7 +96,6 @@ private lateinit var autenticacao : FirebaseAuth
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menusair -> {
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao()
                 autenticacao.signOut()
                 var intent = Intent(this,IntroActivity::class.java)
                 startActivity(intent)
@@ -97,6 +105,40 @@ private lateinit var autenticacao : FirebaseAuth
 
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun recuperarResumo(){
+
+        var emailUsuario = autenticacao.currentUser?.email
+        var idUsuario = Base64Custom.codificarBase64(emailUsuario.toString())
+
+        var usuarioRef:DatabaseReference = firebaseRef.child("usuarios")
+            .child(idUsuario)
+
+
+        usuarioRef.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                var usuario  =  dataSnapshot.getValue<Usuario>()
+
+                despesaTotal = usuario!!.despesaTotal
+                receitaTotal = usuario!!.receitaTotal
+                resumoUsuario = receitaTotal - despesaTotal
+
+                textoSaldacao.text = "Olá" + usuario.nome
+
+                var decimalFormat : DecimalFormat = DecimalFormat("0.##")
+                var resultadoFormatado = decimalFormat.format(resumoUsuario)
+
+                textoSaldo.text = "R$ " +resultadoFormatado
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
     }
 
 }
